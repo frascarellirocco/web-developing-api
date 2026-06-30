0df733c4c1e74af308224d496d728f26a5d6247c
frascarellirocco <frascarellirocco@gmail.com>
2026-06-30 15:07:05 +0000

    progettofilmnuovo

--- progetto/progetto_film.py
+++ progetto/progetto_film.py
@@ -17,3 +17,18 @@ def cerca_film(keyword: str):
     if not risultato:
         raise HTTPException(status_code=404, detail="Film non trovato")
     return risultato
+
+    # ESERCITAZIONE 4: Rotta di dettaglio per il singolo film (Slide 10-11)
+@router.get("/film/{id_film}")
+def dettaglio_film(id_film: int):
+    conn = sqlite3.connect("database.db")
+    conn.row_factory = sqlite3.Row  # Fondamentale per leggere i dati come dizionario
+    cursor = conn.cursor()
+    
+    cursor.execute("SELECT * FROM film WHERE id = ?", (id_film,))
+    risultato = cursor.fetchone()  # Recuperiamo un solo risultato (il singolo film)
+    conn.close()
+    
+    if not risultato:
+        raise HTTPException(status_code=404, detail="Film non trovato")
+    return risultato
--- progetto/progetto_main.py
+++ progetto/progetto_main.py
@@ -5,11 +5,20 @@ from .progetto_db import dbinit
 from .progetto_prodotti import router as prodotti_router
 from .progetto_utente import router as utenti_router
 from .progetto_film import router as film_router
+from fastapi.middleware.cors import CORSMiddleware
 
 # Inizializzazione database all'avvio
 dbinit()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],          # Permette l'accesso da qualsiasi sito (Origin)
+    allow_credentials=True,
+    allow_methods=["*"],          # Permette tutti i metodi (GET, POST, PUT, DELETE, ecc.)
+    allow_headers=["*"],          # Permette tutte le intestazioni (Headers)
+)
 
 # Agganciamo entrambi i router all'applicazione principale (Lezione 7, Slide 5)
 app.include_router(prodotti_router)
